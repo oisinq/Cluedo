cb20f1ac281bcda5ea24f6619561a7d1394dcc75
Oisin Quinn <oisin1001@gmail.com>
2018-01-30 01:18:28 +0000

    Added cluedo_board.jpg and basic graphical interface template

--- src/Cluedo.java
+++ src/Cluedo.java
@@ -1,8 +1,9 @@
 public class Cluedo {
 
-    public static void main(String args[]) {
-        System.out.println("hi");
-        System.out.println("testmctestface");
-        System.out.println("Carracosta");
+    public static void main(String args[]) throws Exception {
+
+        GUI.generateGUI();
+
     }
+
 }
