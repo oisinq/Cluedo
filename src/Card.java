7085299bd38dffb6a3cfa3c4e5db0fbc2240d39c
charleskelly13 <charliekelly1997@hotmail.com>
2018-03-02 21:21:52 +0000

    Card file

---
+++ src/Card.java
@@ -0,0 +1,29 @@
+
+public class Card  {
+
+    private int xLocation, yLocation;
+    // 43, 50 is top of grid
+  
+    private String cardName;
+    private String Type;
+    private Boolean Owned;
+
+    Card(String cardName, String Type, Boolean Owned) {
+    	this.cardName=cardName;
+    	this.Type=Type;
+    	this.Owned=Owned;
+        
+    }
+    public String getName() {
+        return cardName;
+    }
+    public String getType() {
+        return Type;
+    }
+    public Boolean isOwned() {
+        return Owned;
+    }
+    public boolean hasCardName(String characterName) {
+        return this.cardName.toLowerCase().equals(cardName.toLowerCase().trim());
+    }
+}
