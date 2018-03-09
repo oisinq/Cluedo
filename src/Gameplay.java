4ec8ad67256f254a34dd2a19993fdedabda422d4
DarraghClarke <darragh.clarke@ucdconnect.ie>
2018-03-09 10:10:47 +0000

    fixed location of the roller

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -81,7 +81,6 @@ public class Gameplay {
 
         Counter highRoller = null;
         highRoller = rollForOrder(init);
-        frame.appendText(highRoller.getCharacterName()+" rolled the highest with "+ highRoller.getRollForOrder() );
         
         //reassign stuff now
 
@@ -116,6 +115,7 @@ public class Gameplay {
         currentPlayerName = play[0];
         // We display the help command in the infoField and start the first turn
         helpCommand();
+        frame.appendText(highRoller.getCharacterName()+" rolled the highest with "+ highRoller.getRollForOrder() +"so they will go first\n");
         turn();
     }
     
