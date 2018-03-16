519c35b062bd74bb0cc8488aba6b3ef142bd219c
Oisin Quinn <oisin1001@gmail.com>
2018-03-16 16:06:55 +0000

    Fix in GameSetup for the jar file

--- src/GameSetup.java
+++ src/GameSetup.java
@@ -3,10 +3,15 @@
     Members: Oisin Quinn (16314071), Darragh Clarke (16387431), Charlie Kelly (16464276)
     "Aurora Borealis! At this time of year? At this time of day? In this part of the country? Localized entirely within your kitchen?" */
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 
 /**
  * This class allows users to input user name and pick which token they wish to play
@@ -16,6 +21,8 @@ class GameSetup {
 
     int a = 0;
     private Counters counters;
+    private BufferedImage Logo;
+
 
     GameSetup(Counters counters) {
         this.counters = counters;
@@ -26,9 +33,15 @@ class GameSetup {
      * Lets the player select the number of characters
      */
     private void playerCountSelect() {
+        Logo = null;
+        try {
+            Logo = ImageIO.read(this.getClass().getResource("cluedo.png"));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: cannot load board image.");
+        }
         //the below two strings and jOptionPane are used to allow the user to pick how many users will play
         String[] playerCount = (new String[]{"2", "3", "4", "5", "6"});
-        String userChoice = (String) JOptionPane.showInputDialog(null, "Select How Many Players You Want", "Cluedo", JOptionPane.QUESTION_MESSAGE, new ImageIcon("src/cluedo.png"), playerCount, playerCount[0]);
+        String userChoice = (String) JOptionPane.showInputDialog(null, "Select How Many Players You Want", "Cluedo", JOptionPane.QUESTION_MESSAGE, new ImageIcon(Logo), playerCount, playerCount[0]);
 
         if (userChoice == null) {//if the user hits cancel the game is quit
             System.out.println("You have exited the game,Thanks For Playing!");
@@ -60,17 +73,23 @@ class GameSetup {
      * Lets the player select which character to play as and enter their own name
      */
     private void CharacterSelect(int PlayerCount) {
+        Logo = null;
+        try {
+            Logo = ImageIO.read(this.getClass().getResource("cluedo.png"));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Error: cannot load board image.");
+        }
         //this string stores all the possible characters
         String[] Characters = (new String[]{"Plum", "White", "Scarlet", "Green", "Mustard", "Peacock"});
         //this list will store the above string in list format , both are needed for the below loop
         List<String> CharacterList = new ArrayList<>(Arrays.asList(Characters));
         for (int i = 0; i < PlayerCount; i++) {
-            String userName = (String) JOptionPane.showInputDialog(null, "Enter Your Username", "Cluedo", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("src/cluedo.png"), null, "");
+            String userName = (String) JOptionPane.showInputDialog(null, "Enter Your Username", "Cluedo", JOptionPane.INFORMATION_MESSAGE, new ImageIcon(Logo), null, "");
             if (userName == null) {//if the user cancels , exit the code
                 System.out.println("You have exited the game,Thanks For Playing!");
                 System.exit(0);
             }
-            String userChoice = (String) JOptionPane.showInputDialog(null, "Player " + (i + 1) + " Please Select A Character", "Cluedo", JOptionPane.QUESTION_MESSAGE, new ImageIcon("src/cluedo.png"), Characters, Characters[0]);
+            String userChoice = (String) JOptionPane.showInputDialog(null, "Player " + (i + 1) + " Please Select A Character", "Cluedo", JOptionPane.QUESTION_MESSAGE, new ImageIcon(Logo), Characters, Characters[0]);
             if (userChoice == null) {//if the user cancels exit the code
                 System.out.println("You have exited the game,Thanks For Playing!");
                 System.exit(0);
