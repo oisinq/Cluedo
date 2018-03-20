a9ecf7b20eb791cf3a8b121f94e4490247dc1e01
Oisin Quinn <oisin1001@gmail.com>
2018-03-20 18:41:54 +0000

    Improvements to questioning system - can only accuse players who are in the game

--- src/Question.java
+++ src/Question.java
@@ -13,67 +13,30 @@ public class Question {
     }
 
     public void interpretInput(String command) {
+        command = command.toLowerCase();
         if (counter == null) {
             selectCounter(command.toLowerCase());
             if (counter != null) {
                 frame.appendText("Enter the weapon to question:");
+            } else {
+                frame.appendText("Invalid input. Please try again!");
             }
         } else if (weapon == null) {
             selectWeapon(command.toLowerCase());
             if (weapon != null) {
                 frame.appendText("You have accused " + counter.getCharacterName() + " of committing a murder with the " +  weapon.getName() + " in the "+ room.getRoomName());
+            } else {
+                frame.appendText("Invalid input. Please try again!");
             }
         }
     }
 
     //TODO - We probably don't need a switch statement here - get() should return null if the counter doesn't exist
     private void selectCounter(String person) {
-        switch (person) {
-            case "scarlet":
-                counter = Counters.get("Scarlet");
-                break;
-            case "mustard":
-                counter = Counters.get("Mustard");
-                break;
-            case "peacock":
-                counter = Counters.get("Peacock");
-                break;
-            case "green":
-                counter = Counters.get("Green");
-                break;
-            case "white":
-                counter = Counters.get("White");
-                break;
-            case "plum":
-                counter = Counters.get("Plum");
-                break;
-            default:
-                frame.appendText("Invalid entry. Please try again!");
-        }
+        counter = Counters.get(person.substring(0, 1).toUpperCase() + person.substring(1));
     }
 
     private void selectWeapon(String weaponName) {
-        switch (weaponName) {
-            case "dagger":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            case "pistol":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            case "leadpipe":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            case "wrench":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            case "candlestick":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            case "rope":
-                weapon = Weapons.get(weaponName.toLowerCase());
-                break;
-            default:
-                frame.appendText("Invalid entry. Please try again!");
-        }
+        weapon = Weapons.get(weaponName.toLowerCase());
     }
 }
