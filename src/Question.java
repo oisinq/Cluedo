2509eb4c7d60ad491ea6d55d34b2c0288f9a6bca
Oisin Quinn <oisin1001@gmail.com>
2018-03-21 19:08:58 +0000

    Very minor fix

--- src/Question.java
+++ src/Question.java
@@ -1,3 +1,8 @@
+/*  Cluedo - Sprint 4
+    Team: auroraBorealis
+    Members: Oisin Quinn (16314071), Darragh Clarke (16387431), Charlie Kelly (16464276)
+    "Aurora Borealis! At this time of year? At this time of day? In this part of the country? Localized entirely within your kitchen?" */
+
 import javax.swing.*;
 
 public class Question {
@@ -67,7 +72,7 @@ public class Question {
 
     private void confirmReset() {
         frame.resetInfoField();
-        
+
     }
 
     public Room getRoom() {
