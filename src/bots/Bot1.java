5868b7192e1bbaaf9b407e7c9460f2ebf284fd72
Oisin Quinn <oisin1001@gmail.com>
2018-04-13 11:44:14 +0100

    Bot moves to the closest room at the start of the game

--- src/bots/Bot1.java
+++ src/bots/Bot1.java
@@ -20,6 +20,8 @@ public class Bot1 implements BotAPI {
     private Deck deck;
     private int movesLeft;
     private int i = 0;
+    private boolean firstTurn = true;
+    private String path;
 
     public Bot1 (Player player, PlayersInfo playersInfo, Map map, Dice dice, Log log, Deck deck) {
         this.player = player;
@@ -51,36 +53,23 @@ public class Bot1 implements BotAPI {
     }
 
     public String getMove() {
-
         try {
             sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        Coordinates currentPosition = player.getToken().getPosition();
-
-        if (movesLeft == 0) {
+        if (firstTurn) {
+            findFirstPath();
             movesLeft = dice.getTotal();
-//            System.out.println(findNearestDoor(currentPosition, 0));
+            firstTurn = false;
         }
 
+        String move = path.substring(0, 1);
+        path = path.substring(1, path.length());
 
-        if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "u"))) {
-            movesLeft = 0;
-            return "u";
-        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "r"))) {
-            movesLeft = 0;
-            return "r";
-        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "d"))) {
-            movesLeft = 0;
-            return "d";
-        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "l"))) {
-            movesLeft = 0;
-            return "l";
-        }
-
-        return "l";
+        movesLeft--;
+        return move;
     }
 
     public String getSuspect() {
@@ -112,4 +101,49 @@ public class Bot1 implements BotAPI {
         // Add your code here
     }
 
+    private void findFirstPath() {
+
+        Coordinates currentPosition = player.getToken().getPosition();
+        int currRow = currentPosition.getRow();
+        int currCol = currentPosition.getCol();
+
+        Coordinates white = new Coordinates(9, 0);
+        Coordinates green = new Coordinates(14, 0);
+        Coordinates peacock = new Coordinates(23, 6);
+        Coordinates scarlet = new Coordinates(7, 24);
+        Coordinates plum = new Coordinates(23, 19);
+        Coordinates mustard = new Coordinates(0, 17);
+
+        if (currRow == scarlet.getRow() && currCol == scarlet.getCol()) {
+            path = "uuuuuuld";
+        } else if (currRow == white.getRow() && currCol == white.getCol()) {
+            path = "dllddddr";
+        } else if (currRow == green.getRow() && currCol == green.getCol()) {
+            path = "drrddddl";
+        } else if (currRow == peacock.getRow() && currCol == peacock.getCol()) {
+            path = "llllluu";
+        } else if (currRow == plum.getRow() && currCol == plum.getCol()) {
+            path = "lllllldd";
+        } else if (currRow == mustard.getRow() && currCol == mustard.getCol()) {
+            path = "rrrrrruu";
+        }
+    }
+
+    private String moveIntoRoom() {
+        Coordinates currentPosition = player.getToken().getPosition();
+        if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "u"))) {
+            movesLeft = 0;
+            return "u";
+        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "r"))) {
+            movesLeft = 0;
+            return "r";
+        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "d"))) {
+            movesLeft = 0;
+            return "d";
+        } else if (map.isDoor(currentPosition, map.getNewPosition(player.getToken().getPosition(), "l"))) {
+            movesLeft = 0;
+            return "l";
+        }
+        return "x";
+    }
 }
